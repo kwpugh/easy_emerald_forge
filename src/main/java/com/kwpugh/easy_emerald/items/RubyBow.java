3b80cbba0e4e037308151b69d94b5f40081a5b21
kwpugh <kwpugh@192.168.68.58>
2021-03-10 07:56:16 -0600

    updates

--- src/main/java/com/kwpugh/easy_emerald/items/RubyBow.java
+++ src/main/java/com/kwpugh/easy_emerald/items/RubyBow.java
@@ -59,7 +59,7 @@ public class RubyBow extends BowItem
                   ArrowItem arrowitem = (ArrowItem)(itemstack.getItem() instanceof ArrowItem ? itemstack.getItem() : Items.ARROW);
                   AbstractArrowEntity abstractarrowentity = arrowitem.createArrow(worldIn, itemstack, playerentity);
                   abstractarrowentity = customeArrow(abstractarrowentity);
-                  abstractarrowentity.func_234612_a_(playerentity, playerentity.rotationPitch, playerentity.rotationYaw, 0.0F, f * 4.5F, 1.0F);
+                  abstractarrowentity.setDirectionAndMovement(playerentity, playerentity.rotationPitch, playerentity.rotationYaw, 0.0F, f * 4.5F, 1.0F);
                   if (f == 1.0F)
                   {
                      abstractarrowentity.setIsCritical(true);
