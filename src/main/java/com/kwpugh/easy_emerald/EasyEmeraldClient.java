ccce9436e33931a5bc8d1634ac13ad94299bf869
Kevin Pugh <kwpugh@comcast.net>
2021-07-24 21:15:31 -0500

    fix bow animation

--- src/main/java/com/kwpugh/easy_emerald/EasyEmeraldClient.java
+++ src/main/java/com/kwpugh/easy_emerald/EasyEmeraldClient.java
@@ -4,9 +4,12 @@ import com.kwpugh.easy_emerald.init.ItemInit;
 
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
+@Mod.EventBusSubscriber(modid = EasyEmerald.modid, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class EasyEmeraldClient
 {   
     @SubscribeEvent
